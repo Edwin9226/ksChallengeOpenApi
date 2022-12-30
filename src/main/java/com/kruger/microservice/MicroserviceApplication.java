05c6255cd07f717710521d63eb83ff5a1e1a441d
Edwin Pinchao <miguepinchao1992@gmail.com>
2022-12-30 08:55:39 -0600

    Configuracion de OPen API

--- src/main/java/com/kruger/microservice/MicroserviceApplication.java
+++ src/main/java/com/kruger/microservice/MicroserviceApplication.java
@@ -2,11 +2,11 @@ package com.kruger.microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
 
 @SpringBootApplication
-@Component
-public class MicroserviceApplication {
+public class MicroserviceApplication extends WebMvcConfigurationSupport {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MicroserviceApplication.class, args);
